1fc4ac4f87a3ed09f3c6853a2f6eda754ba236ad
woodwardtw <twwoodward@me.com>
2021-03-15 10:43:11 -0400

    acf local json set

--- functions.php
+++ functions.php
@@ -26,6 +26,7 @@ $understrap_includes = array(
 	'/class-wp-bootstrap-navwalker.php',    // Load custom WordPress nav walker. Trying to get deeper navigation? Check out: https://github.com/understrap/understrap/issues/567.
 	'/editor.php',                          // Load Editor functions.
 	'/cpt.php',                          // Load custom post types and taxonomies
+	'/acf.php',                          // Load custom post types and taxonomies
 	'/deprecated.php',                      // Load deprecated functions.
 );
 
--- inc/cpt.php
+++ inc/cpt.php
@@ -65,7 +65,7 @@ function create_presentation_cpt() {
     'show_in_rest' => true,
     'publicly_queryable' => true,
     'capability_type' => 'post',
-    'menu_icon' => 'dashicons-universal-access-alt',
+    'menu_icon' => 'dashicons-welcome-view-site',
   );
   register_post_type( 'presentation', $args );
   
