a4def0aea4216ee992994d9e5f26445ce0340107
woodwardtw <twwoodward@me.com>
2021-06-09 15:38:10 -0400

    change types, add menu to api
    acf logic addition

--- functions.php
+++ functions.php
@@ -115,6 +115,33 @@ function acf_to_rest_api_presenter($response, $post, $request) {
 add_filter('rest_prepare_speaker', 'acf_to_rest_api_presenter', 10, 3);
 
 
+// menu json api via https://stackoverflow.com/a/66157232/3390935
+
+function wp_menu_route() {
+    $menuLocations = get_nav_menu_locations(); // Get nav locations set in theme, usually functions.php)
+    return $menuLocations;
+    }
+
+    add_action( 'rest_api_init', function () {
+        register_rest_route( 'custom', '/menu/', array(
+        'methods' => 'GET',
+        'callback' => 'wp_menu_route',
+    ) );
+} );
+
+function wp_menu_single($data) {
+    $menuID = $data['id']; // Get the menu from the ID
+    $primaryNav = wp_get_nav_menu_items($menuID); // Get the array of wp objects, the nav items for our queried location.
+    return $primaryNav;
+    }
+
+    add_action( 'rest_api_init', function () {
+        register_rest_route( 'custom', '/menu/(?P<id>\d+)', array(
+        'methods' => 'GET',
+        'callback' => 'wp_menu_single',
+    ) );
+} );
+
 
 //CORS 
 
